8cfb1a1bdd9c29789fa70900406247ba0aa53c55
Mario Villacreses <MarioVillacreses@outlook.com>
2018-03-06 12:21:08 -0500

    Integrated nunjucks with res.render

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const fs = require('fs');
+const nunjucks = require('nunjucks');
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  noCache: true
+});
+
+var nunjucksVars = {
+  title: "An Example",
+  people: [
+    {name: 'Gandalf'},
+    {name: 'Frodo'},
+    {name: 'Hermione'}
+  ]
+}
 app.get('/', (req, res, next) => {
-  res.send('Hello world');
+  // res.send('Hello world');
+  res.render('index.html', nunjucksVars);
   next();
 });
 
 app.use('/', (req, res, next) => {
-  fs.writeFile('./logFile.txt', req.toString(), {flag: 'a'}, (err) => {
-    if(err) throw err;
+  fs.writeFile('./logFile.txt', req.toString() + "\n", {flag: 'a'}, (err) => {
+    if(err) {
+      console.log("The error is " + err); 
+      throw err;
+    }
     else console.log('success');
   });
 
